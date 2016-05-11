f978179ac80f3ffecfbf1099dada5ffee05bc093
mcguffin <joern@flyingletters.com>
2016-05-11 14:52:52 +0200

    fix label sr-only wording

--- include/template/grid-view.php
+++ include/template/grid-view.php
@@ -69,7 +69,7 @@ if ( ! defined('ABSPATH') )
 
 	<script type="text/html" id="tmpl-grid-media-modal">
 		<div class="media-modal wp-core-ui">
-			<button type="button" class="button-link media-modal-prev"><span class="dashicons dashicons-arrow-left-alt2"><span class="screen-reader-text"><?php _e('Previous'); ?></span></span></button>
+			<button type="button" class="button-link media-modal-prev"><span class="dashicons dashicons-arrow-left-alt2"><span class="screen-reader-text"><?php _e('Next'); ?></span></span></button>
 			<button type="button" class="button-link media-modal-next"><span class="dashicons dashicons-arrow-right-alt2"><span class="screen-reader-text"><?php _e('Next'); ?></span></span></button>
 			<button type="button" class="button-link media-modal-close"><span class="media-modal-icon"><span class="screen-reader-text"><?php _e('Close'); ?></span></span></button>
 			<div class="media-modal-content"></div>
