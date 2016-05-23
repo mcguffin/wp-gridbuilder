5d15146be91b3300a8c480042ab4c77eba4786bc
mcguffin <joern@flyingletters.com>
2016-05-23 10:36:15 +0200

    fix container sort-handle

--- include/template/container-view.php
+++ include/template/container-view.php
@@ -5,11 +5,13 @@ if ( ! defined('ABSPATH') )
 
 ?>
 	<script type="text/html" id="tmpl-container-view">
+		<div class="top sort-handle"></div>
+		<div class="bottom sort-handle"></div>
 		<a href="#" class="toggle-collapse dashicons dashicons-arrow-down"></a>
 		<div class="display-title"></div>
 		<div class="display-attributes">
 			<span class="display-id"></span>
 			<span class="display-class"></span>
 		</div>
-		<ul class="collection sort-handle rows" data-sort-group="row"></ul>
+		<ul class="collection rows" data-sort-group="row"></ul>
 	</script>
