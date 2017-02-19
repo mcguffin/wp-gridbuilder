c3e90335df8d435227d82f207c0e4b4c629bd1b5
mcguffin <joern@flyingletters.com>
2017-02-19 11:02:52 +0100

    Autoload

--- include/vendor/autoload.php
+++ include/vendor/autoload.php
@@ -3,10 +3,12 @@
 namespace WPGridbuilder;
 
 function __autoload( $class ) {
-	$ds = DIRECTORY_SEPARATOR;
-	$file = GRIDBUILDER_DIRECTORY . 'include' . $ds . str_replace( '\\', $ds, $class ) . '.php';
-	if ( file_exists( $file ) ) {
-		require_once $file;
+	if ( in_array( strpos( $class, __NAMESPACE__ ), array( 0,1 ), true ) ) {
+		$ds = DIRECTORY_SEPARATOR;
+		$file = GRIDBUILDER_DIRECTORY . 'include' . $ds . str_replace( '\\', $ds, $class ) . '.php';
+		if ( file_exists( $file ) ) {
+			require_once $file;
+		}
 	}
 }
 
