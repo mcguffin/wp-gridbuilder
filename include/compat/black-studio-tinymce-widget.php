af5a4d0207cdcf875e5d5c9df331ba11760f8d39
mcguffin <joern@flyingletters.com>
2017-02-19 10:57:35 +0100

    compat

--- include/compat/black-studio-tinymce-widget.php
+++ include/compat/black-studio-tinymce-widget.php
@@ -2,9 +2,9 @@
 
 namespace WPGridbuilder;
 
-function gridbuilder_black_studio_tinymce_enable_pages( $pages ) {
+function black_studio_tinymce_enable_pages( $pages ) {
 	$pages[] = 'post.php';
 	return $pages;
 }
 
-add_filter( 'black_studio_tinymce_enable_pages', 'WPGridbuilder\gridbuilder_black_studio_tinymce_enable_pages' );
+add_filter( 'black_studio_tinymce_enable_pages', 'WPGridbuilder\black_studio_tinymce_enable_pages' );
